37c25b32db2a8e57719d22d996f3c0b811cd04b7
ForsakenAginor <juriys2022@mail.ru>
2024-05-21 14:52:04 +0400

    2

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using Agava.WebUtility;
 
 public class PlayerInput
 {
@@ -7,16 +8,16 @@ public class PlayerInput
 
     public Vector3 GetDirection()
     {
-        if (Input.touchSupported == false)
+        if (Device.IsMobile == false)
             return Vector3.forward * Input.GetAxis(Vertical) + Vector3.right * Input.GetAxis(Horizontal);
 
         if (Input.touchCount == 0)
             return Vector3.zero;
 
-        float verticalCenter = Screen.height / 2;
-        float horizontalCenter = Screen.width / 2;
+        float verticalCenter = Screen.height / 2f;
+        float horizontalCenter = Screen.width / 2f;
         Vector2 touchPosition = Input.GetTouch(0).position;
-        Vector2 direction = touchPosition - new Vector2(horizontalCenter, verticalCenter);
+        Vector2 direction = new Vector2( (touchPosition.x - horizontalCenter) / touchPosition.x , (touchPosition.y - verticalCenter) / touchPosition.y);
 
         return new Vector3(direction.x, 0, direction.y);
     }
