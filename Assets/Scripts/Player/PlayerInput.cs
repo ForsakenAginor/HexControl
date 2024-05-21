c4655d23eb698161767273f224f249b59a9b30bd
ForsakenAginor <juriys2022@mail.ru>
2024-05-21 15:55:39 +0400

    2

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -9,7 +9,7 @@ public class PlayerInput
     public Vector3 GetDirection()
     {
         if (Device.IsMobile == false)
-            return Vector3.forward * Input.GetAxis(Vertical) + Vector3.right * Input.GetAxis(Horizontal);
+            return (Vector3.forward * Input.GetAxis(Vertical) + Vector3.right * Input.GetAxis(Horizontal)).normalized;
 
         if (Input.touchCount == 0)
             return Vector3.zero;
@@ -19,6 +19,6 @@ public class PlayerInput
         Vector2 touchPosition = Input.GetTouch(0).position;
         Vector2 direction = new Vector2( (touchPosition.x - horizontalCenter) / touchPosition.x , (touchPosition.y - verticalCenter) / touchPosition.y);
 
-        return new Vector3(direction.x, 0, direction.y);
+        return new Vector3(direction.x, 0, direction.y).normalized;
     }
 }
