c7a3852c35b3641ed80e9945c74673b2d87b5584
ForsakenAginor <juriys2022@mail.ru>
2024-05-23 16:11:25 +0400

    addYandexLocalizationSwitch

--- Assets/Scripts/BootTrap.cs
+++ Assets/Scripts/BootTrap.cs
@@ -1,10 +1,14 @@
 using Agava.YandexGames;
+using Lean.Localization;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class BootTrap : MonoBehaviour
 {
+    private Dictionary<string, string> _languages = new Dictionary<string, string>() { { "ru", "Russian" }, {"en", "English" }, {"tr", "Turkish" } };
+
     private void Awake()
     {
         YandexGamesSdk.CallbackLogging = true;
@@ -15,11 +19,14 @@ public class BootTrap : MonoBehaviour
         // Always wait for it if invoking something immediately in the first scene.
         while( YandexGamesSdk.IsInitialized == false ) 
             yield return YandexGamesSdk.Initialize();
-        
+
+        string language = YandexGamesSdk.Environment.i18n.lang;
+        LeanLocalization.SetCurrentLanguageAll(_languages[language]);
+        LeanLocalization.UpdateTranslations();
+        /*
         if (PlayerAccount.IsAuthorized == false)
             PlayerAccount.StartAuthorizationPolling(1500);
-        
-        //yield return new WaitForSeconds(5);
+        */
         SceneManager.LoadScene(Scenes.MainMenu.ToString());
     }
 }
