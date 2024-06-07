4d8a4210be396867c9fe1aaa587e1bc2395cf4bd
ForsakenAginor <juriys2022@mail.ru>
2024-06-07 13:39:22 +0400

    HatSkinData refactoring

--- Assets/Scripts/UI/Menu/Profile/Skins/Hatter.cs
+++ Assets/Scripts/UI/Menu/Profile/Skins/Hatter.cs
@@ -7,6 +7,7 @@ public class Hatter
     private Hat _activeHat;
     private List<Hat> _ownedHats = new();
     private IEnumerable<Hat> _hatsList;
+    private HatSkinData _hatSkinData = new();
 
     public Hatter(HatsCollection collection)
     {
@@ -14,8 +15,8 @@ public class Hatter
             throw new ArgumentNullException(nameof(collection));
 
         _hatsList = collection.Hats;
-        _activeHat = _hatsList.First(o => o.Type == HatSkinData.Instance.ActiveHat);
-        var ownedHats = HatSkinData.Instance.OwnedHats;
+        _activeHat = _hatsList.First(o => o.Type == _hatSkinData.ActiveHat);
+        var ownedHats = _hatSkinData.OwnedHats;
         _ownedHats = _hatsList.Where(o => ownedHats.Contains(o.Type)).ToList();
     }
 
@@ -38,7 +39,7 @@ public class Hatter
         hat = stillNotAllowedHats.ToArray()[UnityEngine.Random.Range(0, stillNotAllowedHats.Count())];
         _ownedHats.Add(hat);
         HatAdded?.Invoke(hat);
-        HatSkinData.Instance.SaveChanges(_ownedHats.Select(o =>o.Type), _activeHat.Type);
+        _hatSkinData.OwnedHats = _ownedHats.Select(o => o.Type);
         return true;
     }
 
@@ -52,6 +53,6 @@ public class Hatter
 
         _activeHat = hat;
         ActiveHatChanged?.Invoke();
-        HatSkinData.Instance.SaveChanges(_ownedHats.Select(o => o.Type), _activeHat.Type);
+        _hatSkinData.ActiveHat = _activeHat.Type;
     }
 }
